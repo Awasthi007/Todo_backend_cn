c689dbd47563338fa0da65aae4f18f4277cef13f
Shashank <awasthi007here@gmail.com>
2022-02-14 22:49:38 +0530

    setup the view engine ejs

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express();
 const port = 8000;
 // use express router
 
+
+//setup the view engine
+
 app.use('/', require('./routes'));
+app.set('view engine', 'ejs');
+app.set('views','./views');
 
 app.listen(port, function(error){
     if(error)
