af2aaf7660388b773b0c15eb905c949462737fa7
Shashank <awasthi007here@gmail.com>
2022-02-14 19:06:07 +0530

    there were some errors----> add controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(request, response){
+    return response.end('<h1> express is up for todo</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+//const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
