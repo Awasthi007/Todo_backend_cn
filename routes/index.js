a8d73fe0309038f5c7b99b04669564ca687c8b31
Shashank <awasthi007here@gmail.com>
2022-02-14 18:51:24 +0530

    routers and controllers added

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const { response } = require('express');
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+//console.log("rorrrr");
+router.get('/', homeController.home );
+
+module.exports = router;
